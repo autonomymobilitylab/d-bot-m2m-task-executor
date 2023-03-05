5a1e37a6e85412b95a1517510d24767133af128d
NkLapp <34657611+NkLapp@users.noreply.github.com>
2023-03-05 16:12:43 +0200

    task priority queue for task scheduling

--- src/task_manager.py
+++ src/task_manager.py
@@ -1,6 +1,9 @@
 import rospy
 from std_msgs.msg import String
 
+from src.priority_queue import TaskPriorityQueue
+
+task_priority_queue = TaskPriorityQueue()
 
 if __name__ == '__main__':
-	rospy.init_node('task_manager')
+    rospy.init_node('task_manager')
