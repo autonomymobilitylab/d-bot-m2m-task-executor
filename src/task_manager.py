b25e0cb66f104d47a250129f1ab1268b3609a6a3
Niko Lappi <niko.lappi@aalto.fi>
2023-03-11 12:35:21 +0200

    initial add task service

--- src/task_manager.py
+++ src/task_manager.py
@@ -3,9 +3,16 @@ from std_msgs.msg import String
 
 from src.util.priority_queue import TaskPriorityQueue
 from src.util.priority_manager import TaskPriorityManager
+from src.api.task import Task
 
 task_priority_queue = TaskPriorityQueue()
 task_priority_manager = TaskPriorityManager()
 
+def add_task_to_queue(task_id):
+    task = Task(task_id,task_priority_manager.get_priority(task_id))
+    task_priority_queue.add_task(task)
+
 if __name__ == '__main__':
     rospy.init_node('task_manager')
+    s = rospy.Service('add_task', task_manager, add_task_to_queue)
+    rospy.spin()
