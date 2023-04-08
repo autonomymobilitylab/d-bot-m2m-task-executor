3bb8bf97722f2ca68931b4bf5b7ec8a4cdb1ded9
NkLapp <34657611+NkLapp@users.noreply.github.com>
2023-04-08 11:13:18 +0300

    initial bot location logging until navigation publisher

--- src/db/logger.py
+++ src/db/logger.py
@@ -1,12 +1,14 @@
-
+from db.postgres_connector import PostgresConnector
 
 class Logger:
-    def __init__(self, db):
+    def __init__(self, db:PostgresConnector):
         self.db = db
 
-    def logLocation(self):
-        # TODO
-        print('Implement this')
+    def log_location(self, location):
+        query = f"INSERT INTO public.dbot_location_log VALUES(1, 'task_executor_logging', '', '{location}'::jsonb);"
+        self.db.connect()
+        self.db.execute(query)
+        self.db.disconnect()
         return None
 
     def logAction(self, ID=None):
